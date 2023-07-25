e337396bc9066ffc83db926056374c8a48221171
mohamedMRG <mohamad1325@gmail.com>
2023-07-25 12:13:21 -0700

    dddd

--- _printf.c
+++ _printf.c
@@ -5,11 +5,67 @@
  * _putchar - Writes a character to stdout.
  * @c: The character to be written.
  * Return: On success, the number of characters written.
- * On error, -1 is returned, and errno is set appropriately.
+ *         On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
 	return write(1, &c, 1);
+}
+
+/**
+ * print_binary - Print an unsigned int in binary format.
+ * @n: The unsigned int to print.
+ * Return: The number of characters printed.
+ */
+int print_binary(unsigned int n)
+{
+	int count = 0;
+	unsigned int mask = 1 << ((sizeof(unsigned int) * 8) - 1);
+
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		count++;
+		mask >>= 1;
+	}
+
+	return count;
+}
+
+/**
+ * print_hex - Print an unsigned int in hexadecimal format.
+ * @n: The unsigned int to print.
+ * @uppercase: Whether to use uppercase or lowercase hex digits.
+ * Return: The number of characters printed.
+ */
+int print_hex(unsigned int n, int uppercase)
+{
+	int count = 0;
+	char *hex_digits_lowercase = "0123456789abcdef";
+	char *hex_digits_uppercase = "0123456789ABCDEF";
+	char *hex_digits = uppercase ? hex_digits_uppercase : hex_digits_lowercase;
+	int digit;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+		while (n > 0)
+		{
+			digit = n % 16;
+			_putchar(hex_digits[digit]);
+			count++;
+			n /= 16;
+		}
+	}
+
+	return count;
 }
 
 /**
@@ -50,6 +106,19 @@ int _printf(const char *format, ...)
 				format++;
 				count++;
 			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+				format++;
+			}
 			else if (*format == 'd' || *format == 'i')
 			{
 				int num = va_arg(args, int);
@@ -71,17 +140,52 @@ int _printf(const char *format, ...)
 				}
 				format++;
 			}
-			else if (*format == 's')
+			else if (*format == 'u')
 			{
-				char *str = va_arg(args, char*);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
+				unsigned int num = va_arg(args, unsigned int);
+				int divisor = 1;
+				while (num / divisor > 9)
+					divisor *= 10;
+				while (divisor > 0)
 				{
-					_putchar(*str);
-					str++;
+					_putchar((num / divisor) + '0');
 					count++;
+					num %= divisor;
+					divisor /= 10;
 				}
+				format++;
+			}
+			else if (*format == 'o')
+			{
+				unsigned int num = va_arg(args, unsigned int);
+				int divisor = 1;
+				while (num / divisor > 7)
+					divisor *= 8;
+				while (divisor > 0)
+				{
+					_putchar((num / divisor) + '0');
+					count++;
+					num %= divisor;
+					divisor /= 8;
+				}
+				format++;
+			}
+			else if (*format == 'x')
+			{
+				unsigned int num = va_arg(args, unsigned int);
+				count += print_hex(num, 0);
+				format++;
+			}
+			else if (*format == 'X')
+			{
+				unsigned int num = va_arg(args, unsigned int);
+				count += print_hex(num, 1);
+				format++;
+			}
+			else if (*format == 'b')
+			{
+				unsigned int num = va_arg(args, unsigned int);
+				count += print_binary(num);
 				format++;
 			}
 			else
