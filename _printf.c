e9f422c41921b63e639bd9df942c5aca9a45aa34
mohamedMRG <mohamad1325@gmail.com>
2023-07-25 11:31:02 -0700

    d

--- _printf.c
+++ _printf.c
@@ -18,11 +18,12 @@ int _putchar(char c)
  * @format:f
  * @count:c
  */
-int printchar(va_list *args, int *count)
+int printchar(va_list *args, const char *format,int *count)
 {
 	char c = va_arg(*args, int);
 	_putchar(c);
 	*count = *count + 1;
+	*format = *format + 1;
 	return (*count);
 }
 /**
@@ -55,8 +56,7 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				printchar(&args, &count);
-				format++;
+				printchar(&args, format, &count);
 			}
 			else if (*format == 's')
 			{
